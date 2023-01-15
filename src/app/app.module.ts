8b23042ce6631777f3dde72e7f62cecea76feea3
minhson.tran <MinhSon.Tran@fdmgroup.com>
2023-01-14 21:40:26 -0500

    Working with Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    FavoriteDirective
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/media-item-form/media-item-form.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.css']
+})
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({});
+  }
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      medium: new FormControl('Movie'),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
+    });
+  }
+
+  yearValidator(control: AbstractControl) {
+    if(control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if(year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return {
+        year: {
+        min: minYear,
+        max: maxYear
+        }
+      };
+    }
+  }
+
+  onSubmit(mediaItem : any) {
+    console.log(mediaItem);
+  }
+
+
+}
